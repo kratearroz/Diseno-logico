265eb0c770d7f926f5ec731832653e45ace067f2
Krate <castanojose158@gmail.com>
2025-09-29 22:01:10 -0500

    Vista

--- src/Vista/Vista.java
+++ src/Vista/Vista.java
@@ -3,11 +3,21 @@ package Vista;
 import java.util.Scanner;
 
 public class Vista {
-     public int pedirdatoInt(String texto){
-         Scanner teclado=new Scanner(System.in);
-         System.out.println(texto);
-         teclado.nextLine();
+    public int pedirdatoInt(String texto) {
+        Scanner teclado = new Scanner(System.in);
+        System.out.println(texto);
+        teclado.nextLine();
         return 0;
-     }
-    
+    }
+
+    public String pedirdatoString(String texto) {
+        Scanner teclado = new Scanner(System.in);
+        System.out.println(texto);
+        return teclado.nextLine();
+    }
+    public String mostrarTexto(String texto) {
+        System.out.println(texto);
+        return texto;
+    }
+
 }
