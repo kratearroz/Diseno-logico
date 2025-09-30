18e7766915d3e75371fe6ebffe3b526da482b598
Krate <castanojose158@gmail.com>
2025-09-29 21:59:08 -0500

    PedirDatoInt

--- src/Vista/Vista.java
+++ src/Vista/Vista.java
@@ -1,5 +1,13 @@
 package Vista;
 
+import java.util.Scanner;
+
 public class Vista {
+     public int pedirdatoInt(String texto){
+         Scanner teclado=new Scanner(System.in);
+         System.out.println(texto);
+         teclado.nextLine();
+        return 0;
+     }
     
 }
