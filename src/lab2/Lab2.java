96ddac93abcac0da0642a64b914b4b6b3488ebd7
Krate <castanojose158@gmail.com>
2025-09-29 20:50:29 -0500

    prueba

--- src/lab2/Lab2.java
+++ src/lab2/Lab2.java
@@ -15,7 +15,7 @@ public class Lab2 {
      */
     public static void main(String[] args) {
         // TODO code application logic her
-        System.out.print("hola soy jose");
+        
     }
     
 }
